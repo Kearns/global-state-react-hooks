49420f128da3a9d8620c11b2e954244bca9d92fc
Kearns <patk4000@gmail.com>
2019-02-17 21:53:46 -0600

    cleaned up hooks

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -2,25 +2,25 @@ import React, { Dispatch } from "react";
 import { StateContext, DispatchContext } from "./contexts";
 import { getOr } from "lodash/fp";
 
-export const useGlobalDispatch = () => {
-  return React.useContext(DispatchContext);
-};
-
 interface IUseGlobalState {
-  getState: (path: string, defaultValue?: any) => IGlobalState;
+  getState: (path: string, defaultValue?: any) => GlobalState;
   setState: (path: string, value: any) => void;
   addStore: (name: string, value: any) => void;
 }
 
-interface IGlobalState {
+type GlobalState = {
   [key: string]: any;
-}
+};
+
+export const useGlobalDispatch = (): React.Dispatch<any> => {
+  return React.useContext(DispatchContext);
+};
 
 export const useGlobalState = (): IUseGlobalState => {
   const state = React.useContext(StateContext);
   const dispatch = useGlobalDispatch();
   return {
-    getState: (path: string, defaultValue: any = undefined): IGlobalState =>
+    getState: (path: string, defaultValue: any = undefined): GlobalState =>
       getOr(defaultValue, path, state),
     setState: (path: string, value: any): void =>
       dispatch({
